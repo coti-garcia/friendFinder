809e789762ab66aa3dd93bc4c92bdb21539a712f
coti_garcia <javiera.garcialatorre@gmail.com>
2019-08-21 17:11:02 -0400

    array scores parceInt

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const path = require("path");
-const PORT = 7000;
+
 const app = express();
+const PORT = 7000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.use( express.static ( path.join( __dirname, "app/public") ) );
