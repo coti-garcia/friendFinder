99ff56fdff9d1f02c3b986d1ba506fd195d45f8c
coti_garcia <javiera.garcialatorre@gmail.com>
2019-08-22 17:06:56 -0400

    change match function to apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,40 @@
-const path = require ("path");
+const path = require("path");
 
-const friends = require ("../data/friends")
-const routes = function(app){
-    app.get("/api/friends", function(req, res){
+const friends = require("../data/friends");
+
+const routes = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
+        console.log(res)
     });
-    app.post("/api/friends", function(req,res){
+    app.post("/api/friends", function (req, res) {
         console.log(req.body)
-        for (let i in req.body.scores) {
-            req.body.scores[i] = parseInt(req.body.scores[i])
-        }
-        friends.push(req.body);
-        res.end();
+        const newFriend = req.body;
+        newFriend.scores.forEach((element, i) => {
+            newFriend.scores[i] = parseInt(newFriend.scores[i])
+        })
+        const totalDiferrenceArray = [];
+
+        friends.forEach(element => {
+            const difference = []
+            for (let i = 0; i < 10; i++) {
+                difference.push(Math.abs(element.scores[i] - newFriend.scores[i]))
+            }
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            const totalDifference = difference.reduce(reducer)
+            totalDiferrenceArray.push(totalDifference)
+            console.log(element.name + " Total Difference:" + totalDifference)
+        });
+        console.log(totalDiferrenceArray);
+        const minDifference = Math.min(...totalDiferrenceArray);
+        const indexOfFriends = totalDiferrenceArray.indexOf(minDifference);
+        console.log(minDifference);
+        const match = friends[indexOfFriends]
+        console.log("Match:" + match.name);
+
+
+        friends.push(newFriend);
+        res.json(match);
     });
 }
 
