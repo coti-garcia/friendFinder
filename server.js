566bcaf850f1e8e5d2c9c5332f4a057591e1e2f2
coti_garcia <javiera.garcialatorre@gmail.com>
2019-08-29 16:26:20 -0400

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 
 // Sets up the Express app to handle data parsing
